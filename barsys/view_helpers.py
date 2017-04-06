7c09ceb38261cc1c305eecb9311fd669bdc0e0de
Nicolai Spohrer <nicolai@xeve.de>
2017-04-06 16:51:55 +0200

    Slight refactoring: created view_helpers.py

---
+++ barsys/view_helpers.py
@@ -0,0 +1,47 @@
+from .models import StatsDisplay, Purchase
+from django.utils import timezone
+
+
+def get_renderable_stats_elements():
+    """Create a list of dicts for all StatsDisplays that can be rendered by view more easily"""
+    stats_elements = []
+
+    all_displays = StatsDisplay.objects.order_by("-show_by_default")
+    for index, stat in enumerate(all_displays):
+        stats_element = {"stats_id": "stats_{}".format(stat.pk),
+                         "show_by_default": stat.show_by_default,
+                         "title": stat.title}
+
+        # construct query filters step by step
+        filters = {}
+        if stat.filter_category:
+            filters["product_category"] = stat.filter_category.name
+        if stat.filter_product:
+            filters["product_name"] = stat.filter_product.name
+
+        # filter by timedelta
+        filters["created_date__gte"] = timezone.now() - stat.time_period
+
+        stats_element["rows"] = []
+        if stat.sort_by_and_show == StatsDisplay.SORT_BY_NUM_PURCHASES:
+            top_five = Purchase.objects.purchases_by_user(**filters)[:5]
+            for user, total_quantity in top_five:
+                stats_element["rows"].append({"left": "{}x".format(total_quantity),
+                                              "right": "{} {}".format(stat.row_string, user.display_name)})
+        else:
+            top_five = Purchase.objects.cost_by_user(**filters)[:5]
+            for u_index, (user, total_cost) in enumerate(top_five):
+                stats_element["rows"].append({"left": "{}.".format(u_index + 1),
+                                              "right": "{} {}".format(stat.row_string, user.display_name)})
+
+        if index + 1 < len(all_displays):
+            # toggle next one on
+            stats_element["toggle_other_on"] = "stats_{}".format(all_displays[index + 1].pk)
+        else:
+            # toggle first one on
+            stats_element["toggle_other_on"] = "stats_{}".format(all_displays[0].pk)
+        # if there is only one display, it's toggled off and on again with one click
+
+        stats_elements.append(stats_element)
+
+    return stats_elements
--- barsys/views.py
+++ barsys/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, redirect
-from .models import Product, Category, User, Purchase, StatsDisplay
+from .models import Product, Category, User, Purchase
 from .forms import SingleUserSinglePurchaseForm
-import datetime
-from django.utils import timezone
-from django.db import models
-from collections import defaultdict
+from .view_helpers import get_renderable_stats_elements
 
 
 def user_purchase(request, user_id):
@@ -50,7 +47,9 @@ def user_list(request):
 
     sidebar_stats_elements = get_renderable_stats_elements()
 
-    context = {"favorites": favorite_users, "all_users": all_users, "last_purchases": last_purchases,
+    context = {"favorites": favorite_users,
+               "all_users": all_users,
+               "last_purchases": last_purchases,
                "sidebar_stats_elements": sidebar_stats_elements}
     return render(request, 'barsys/user_list.html', context)
 
@@ -63,56 +62,7 @@ def user_history(request, user_id):
 
     last_purchases = Purchase.objects.filter(user__pk=user.pk).order_by("-created_date")[:10]
 
-    context = dict()
-    context["user"] = user
-    context["categories"] = categories
-    context["last_purchases"] = last_purchases
+    context = {"user": user,
+               "categories": categories,
+               "last_purchases": last_purchases}
     return render(request, "barsys/user_history.html", context)
-
-
-# Helpers
-
-def get_renderable_stats_elements():
-    """Create a list of dicts for all StatsDisplays that can be rendered by view more easily"""
-    stats_elements = []
-
-    all_displays = StatsDisplay.objects.order_by("-show_by_default")
-    for index, stat in enumerate(all_displays):
-        stats_element = dict()
-        stats_element["stats_id"] = "stats_{}".format(stat.pk)
-        stats_element["show_by_default"] = stat.show_by_default
-        stats_element["title"] = stat.title
-
-        # construct query filters step by step
-        filters = dict()
-        if stat.filter_category:
-            filters["product_category"] = stat.filter_category.name
-        if stat.filter_product:
-            filters["product_name"] = stat.filter_product.name
-
-        # filter by timedelta
-        filters["created_date__gte"] = timezone.now() - stat.time_period
-
-        stats_element["rows"] = []
-        if stat.sort_by_and_show == StatsDisplay.SORT_BY_NUM_PURCHASES:
-            top_five = Purchase.objects.purchases_by_user(**filters)[:5]
-            for user, total_quantity in top_five:
-                stats_element["rows"].append({"left": "{}x".format(total_quantity),
-                                              "right": "{} {}".format(stat.row_string, user.display_name)})
-        else:
-            top_five = Purchase.objects.cost_by_user(**filters)[:5]
-            for u_index, (user, total_cost) in enumerate(top_five):
-                stats_element["rows"].append({"left": "{}.".format(u_index + 1),
-                                              "right": "{} {}".format(stat.row_string, user.display_name)})
-
-        if index + 1 < len(all_displays):
-            # toggle next one on
-            stats_element["toggle_other_on"] = "stats_{}".format(all_displays[index + 1].pk)
-        else:
-            # toggle first one on
-            stats_element["toggle_other_on"] = "stats_{}".format(all_displays[0].pk)
-        # if there is only one display, it's toggled off and on again with one click
-
-        stats_elements.append(stats_element)
-
-    return stats_elements
