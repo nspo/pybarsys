bad1601d499c4f9939a1f6b1d8bed47793bd13d6
Nicolai <nicolai@xeve.de>
2018-12-19 08:58:30 +0100

    Update settings.py production example

--- pybarsys/settings.py
+++ pybarsys/settings.py
@@ -1,4 +1,4 @@
 # Create production settings at settings_production.py and change the import command below
 
 from ._settings.dev import *
-# from ._settings.production import *
+# from ._settings.production_yourbar import *
